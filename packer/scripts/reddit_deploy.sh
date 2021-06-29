ec6138bad93959aeec3b068652750e2119686c30
Evgeny Lapshin <leon4eg.eo@gmail.com>
2021-06-29 22:38:05 +0300

    fix immutable deploy

--- packer/scripts/reddit_deploy.sh
+++ packer/scripts/reddit_deploy.sh
@@ -4,6 +4,7 @@ echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" 
 apt-get update
 sleep 10
 apt-get install -y ruby-full ruby-bundler build-essential mongodb-org git
+cp /tmp/reddit-app.service /etc/systemd/system/
 systemctl daemon-reload
 systemctl start mongod
 systemctl enable mongod
