5289f90823f91a34d253005dce0e92a5b3e0787e
Evgeny Lapshin <leon4eg.eo@gmail.com>
2021-07-05 12:19:45 +0300

    fix terraform/main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,6 @@
+provider "yandex" {
+  token     = "AQAAAABVxFUiAATuwWfEocaPukmWuTfr2T_6g60"
+  cloud_id  = "b1gknifmdqqekr93bpo2"
+  folder_id = "b1g2v6pvj8d70ian4qfn"
+  zone      = "ru-central1-a"
+}
