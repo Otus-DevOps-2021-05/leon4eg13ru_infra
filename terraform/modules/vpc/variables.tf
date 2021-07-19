b28eea83f871905da000503df04af7deed8203ec
Evgeny Lapshin <leon4eg.eo@gmail.com>
2021-07-19 16:28:56 +0300

    fix vpc module

---
+++ terraform/modules/vpc/variables.tf
@@ -0,0 +1,15 @@
+variable "subnet_name" {
+  description = "Sabnet name"
+}
+variable zone {
+  description = "Zone"
+  default     = "ru-central1-a"
+}
+variable "network_id" {
+  description = "network id"
+  default     = "${yandex_vpc_network.app-network.id}"
+}
+variable v4_cidr_blocks {
+  description = "subnet Ip address pool"
+  default     = "192.168.10.0/24"
+}
