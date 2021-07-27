4602550e35e49b32a1f67b78d8a647b1d494d4ed
Evgeny Lapshin <leon4eg.eo@gmail.com>
2021-07-27 23:36:30 +0300

    fix packer deploy

--- ansible/autoinv.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-s3cmd get s3://leon4eg13ru-storage-bucket/stage/terraform.tfstate terraform.tfstate --force
-APPSERVER=$(cat terraform.tfstate | jq '.outputs.external_ip_address_app.value')
-DBSERVER=$(cat terraform.tfstate | jq '.outputs.external_ip_address_db.value')
-echo '{"all":{"children":{"app":{"hosts":{'$APPSERVER':null}},"db":{"hosts":{'$DBSERVER':null}}}}}' | jq '.' > inventory.json
-rm terraform.tfstate
